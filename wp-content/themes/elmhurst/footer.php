405080b0368264a291d2836495919895c2e073ed
Terry Fadz <tfadz@suntimes.com>
2018-04-19 17:03:53 -0500

    updated footer

--- wp-content/themes/elmhurst/footer.php
+++ wp-content/themes/elmhurst/footer.php
@@ -14,18 +14,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'elmhurst' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'elmhurst' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'elmhurst' ), 'elmhurst', '<a href="http://underscores.me/">Terry Fadz</a>' );
-				?>
+		<?php
+			wp_nav_menu( array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'primary-menu',
+			) );
+		?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
